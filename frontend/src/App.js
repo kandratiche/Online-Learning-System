1f583f479fbcbcdc1c00dff78d52a12c9a73d9c4
kandratiche <cgnipnoob@gmail.com>
2025-09-25 22:33:42 +0500

    What?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import SingUp from './pages/SignUp';
 import Login from './pages/Login';
 import AllCourses from './pages/AllCourses';
 
-const API = axios.create({ baseURL: `${process.env.API_URL}` });
+const API = axios.create({ baseURL: `${process.env.API_URL}/api` });
 
 export const getCourses = () => API.get("/courses");
 export const getCourseById = (_id) => API.get(`/courses/${_id}`);
