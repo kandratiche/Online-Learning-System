28e0d0aafb3a6b67dc69d6672a94f20c77dcd28e
kandratiche <cgnipnoob@gmail.com>
2025-10-18 00:12:16 +0500

    aga

--- frontend/src/components/Support.tsx
+++ frontend/src/components/Support.tsx
@@ -3,6 +3,8 @@ export default function Support() {
         <div className="flex flex-col w-315 max-[768px]:w-75 max-[768px]:ml-0 bg-zinc-200 rounded-xl mt-10 ml-10 p-6 dark:bg-black dark:text-white">
             <p className="text-2xl font-bold">Support</p>
             <p className="mt-5">Contact us</p>
+            <p className="ml-5 text-bold">telegram</p>
+            <p className="ml-5 text-bold">github</p>
         </div>
     )
 }
